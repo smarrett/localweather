23ad2c40f6f3d228d7c167c7af91f3d3d25a3161
Scott Marrett <33820193+smarrett@users.noreply.github.com>
2019-04-09 16:25:45 -0600

    Add files via upload

--- darkskycall.py
+++ darkskycall.py
@@ -4,16 +4,28 @@ import requests
 import urllib.request
 import csv
 import time
+import datetime
 import json
 
 # locations to retrieve weather data for
-locations = {'moab, ut': [38.5733, -109.5498], 'niwot, co': [40.1039, -105.1708], 'fairplay, co': [39.2360, -106.1059]}
+# locations = {'moab, ut': [38.5733, -109.5498], 'niwot, co': [40.1039, -105.1708], 'fairplay, co': [39.2360, -106.1059]}
+
+# read locations from csv
+with open ('weather_locations.csv') as csvfile:
+	readCSV = csv.reader(csvfile, delimiter=',')
+	locations = {}
+	for row in readCSV:
+		city = row[0]
+		state = row[1]
+		latitude = row[2]
+		longitude = row[3]
+		locations[city + ', ' + state] = [latitude, longitude]
 
 # dark sky API key and URL
 darkskyKey = '5fc19b8698057d776e621b39c203f1f6'
 darkskyURLBase = 'https://api.darksky.net/forecast/'
 
-# function to retrieve weather data via Dark Sky API
+# function to retrieve weather data via Dark Sky API for each location
 def get_weather_data(latitude, longitude):
 	api_url = darkskyURLBase + darkskyKey + '/' + str(latitude) + ',' + str(longitude)
 	response = requests.get(api_url)
@@ -23,6 +35,11 @@ def get_weather_data(latitude, longitude):
 
 	else:
 		return None
+
+# create new csv file and write headers
+with open('todays_weather_data ' + datetime.datetime.today().strftime('%d-%m-%Y') + '.csv', mode = 'a') as weather_file:
+		weather_writer = csv.writer(weather_file, delimiter = ',', quotechar = '"')
+		weather_writer.writerow(['location', 'current summary', 'current temp', 'current wind speed', 'today summary', 'today high temp', 'today low temp', 'today sunrise', 'today sunset'])
 
 # for loop to pull weather data for each location in dictionary
 for location in locations:
@@ -48,8 +65,7 @@ for location in locations:
 	nextSunset = time.strftime('%H:%M', time.localtime(nextSunsetSysTime))
 
 	#write data to csv
-	with open(location + '_data.csv', mode = 'w') as weather_file:
+	with open('todays_weather_data ' + datetime.datetime.today().strftime('%d-%m-%Y') + '.csv', mode = 'a') as weather_file:
 		weather_writer = csv.writer(weather_file, delimiter = ',', quotechar = '"')
-
-		weather_writer.writerow([currentSum, currentTemp, currentWind, dailySum, todaySum, tempHigh, tempLow, nextSunrise, nextSunset])
+		weather_writer.writerow([location, currentSum, currentTemp, currentWind, dailySum, todaySum, tempHigh, tempLow, nextSunrise, nextSunset])
 
